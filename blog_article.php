872bd6469227dd66a0efb26ea3cb95df5726411a
sergey-h <ssd.hogan@gmail.com>
2016-03-03 11:04:04 +0300

    Blog Article added

---
+++ blog_article.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>Welcome to Credit1Solutions.com</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap theme -->
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+  <link href="css/font-awesome.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+
+  <link href="scss/parts/fonts.css" rel="stylesheet">
+
+
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+
+  <link href="scss/parts/overall.css" rel="stylesheet">
+  <link href="scss/parts/header.css" rel="stylesheet">
+  <link href="scss/parts/footer.css" rel="stylesheet">
+
+  <link href="scss/pages/blog.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script src="js/jquery.min.js"></script>
+</head>
+
+<body role="document" class='page-blog'>
+
+  <!-- Fixed navbar -->
+  <nav class="navbar">
+    <div class="brdrbtm">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-4 navbar-block">
+            <a class="navbar-brand" href="./"><img class="logo" src="images/logo.png" title="Credit1Solutions.com" alt="Credit1Solutions.com" /></a>
+          </div>
+          <div class="col-sm-8 navbar-block">
+            <div class="colnowsec flright">
+              <div class="callnow"><span class="telicon"></span> call now <span class="telno">1-877-782-7839</span></div>
+              <div class="wrap_getstart"><a href="#" class="getstarted">GET STARTED</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="flleft">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">SERVICES</a></li>
+                <li><a href="./">MILITARY DISCOUNTS</a></li>
+                <li><a href="./">CREDIT EDUCATION</a></li>
+              </ul>
+
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="flright search-block">
+            <div class="client-login">click here for the <span>Client Login</span></div>
+            <div class="search"><div id="input"><input type="search" name="search-terms" id="search-terms" placeholder="Enter search terms..."></div><span class="fa fa-search" id="label"></span></div>
+
+          </div>
+        </div>
+      </div><!-- end row-->
+
+    </div>
+  </nav>
+
+  <section class="breadcrumbs"> 
+    <div class="container">
+      <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Blog</li>
+      </ol>
+    </div>
+  </section>
+
+  <section class="page-heading">
+    <div class="container">
+      <h1 class="title-h1">Thousands of Satisfied Customers.</h1>
+      <div class="media media-style1">
+        <div class="media-left hidden-xs">
+          <a href="#">
+            <img class="media-object" src="images/header-blog.png" alt="">
+          </a>
+        </div>
+        <div class="media-body">
+          <p>We can help! We have helped others just like you improve their credit rating and we can improve your credit rating too. It's easy to get started. Just look at all of these happy customers. They're glad they called Credit1Solutions.com Solutions, so why haven't you? </p>        
+        </div>
+      </div>      
+    </div>
+  </section>
+
+  <section class="strapline">
+    <div class="container">
+      Credit Report Repair Questions? <span>Chat with a Certified FICO Expert Now: 1-877-782-7839</span>
+    </div>
+  </section>
+
+
+  <section class="blog-items">
+    <div class="container">
+
+      <div class="blog-item">
+        <div class="row">
+          <div class="col-sm-4 blog-item-img">
+            <img src="images/tmp/blog_1.jpg" alt="">
+          </div>
+          <div class="col-sm-8 blog-item-content">
+            <h1 class="title-h1">Credit1Solutions.com can help repair your bad credit</h1>
+            <div class="blog-item-header">
+              <ol class="breadcrumb pull-left">
+                <li><a href="#"><em>By Paul Chambers</em></a></li>
+                <li><a href="#"><em>May 27, 2015</em></a></li>
+                <li><a href="#"><em>4 Comments</em></a></li>
+              </ol>                
+            </div>
+            <p>The climb back to financial freedom can be frustrating due to bad credit. Although you can do it yourself, the process of credit repair can be very difficult. It requires a lot of record-keeping, great communication skills, great knowledge of federal and state consumer laws, and will require time. Many people have difficulties with credit repair simply because they do not understand all that is involved.</p>
+
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <div class="blog-item-footer">      
+              <div class="blog-item-share pull-right">
+                Share : 
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+              </div>      
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="container">
+      <div class="comments">  
+        <div class="page-header comments-header">
+          4 Comments
+        </div> 
+
+        <div class="comment">
+          <div class="media">
+            <div class="media-left comment-avatar">
+              <img class="media-object" src="images/tmp/ava1.jpg" >
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading comment-title text-uppercase">akhil Designer</h4>
+              <div class="comment-date">May 28, 2015 at 9:44 pm</div>
+              <div class="comment-content">
+                <p>Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature</p>
+              </div>
+              <a href="#" class="comment-reply text-uppercase">Reply</a>
+
+              <div class="comment">
+                <div class="media">
+                  <div class="media-left comment-avatar">
+                    <img class="media-object" src="images/tmp/ava2.jpg" >
+                  </div>
+                  <div class="media-body">
+                    <h4 class="media-heading comment-title text-uppercase">sunil joshi</h4>
+                    <div class="comment-date">May 28, 2015 at 6:29 pm</div>
+                    <div class="comment-content">
+                      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney.</p>
+                    </div>
+                    <a href="#" class="comment-reply text-uppercase">Reply</a>
+                  </div>
+                </div>          
+              </div> 
+
+              <div class="comment">
+                <div class="media">
+                  <div class="media-left comment-avatar">
+                    <img class="media-object" src="images/tmp/ava3.jpg" >
+                  </div>
+                  <div class="media-body">
+                    <h4 class="media-heading comment-title text-uppercase">Sonia nair</h4>
+                    <div class="comment-date">May 28, 2015 at 4:27 pm</div>
+                    <div class="comment-content">
+                      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators.</p>
+                    </div>
+                    <a href="#" class="comment-reply text-uppercase">Reply</a>
+                  </div>
+                </div>          
+              </div> 
+
+
+            </div>
+          </div>          
+        </div>  
+
+        <div class="comment">
+          <div class="media">
+            <div class="media-left comment-avatar">
+              <img class="media-object" src="images/tmp/ava4.jpg" >
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading comment-title text-uppercase">krishnan unni</h4>
+              <div class="comment-date">May 28, 2015 at 3:14 pm</div>
+              <div class="comment-content">
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+              </div>
+              <a href="#" class="comment-reply text-uppercase">Reply</a>
+            </div>
+          </div>          
+          </div>         
+      </div>
+    </div>
+
+    <div class="comment-form">
+      <div class="container">
+        <span class="fa fa-comments-o"></span>
+        <h5 class="text-uppercase">Leave a</h5>
+        <h3 class="text-uppercase">Comment</h3>
+        
+        <form>          
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="form-group">
+                <input type="text" class="form-control" id="fname" placeholder="Name">
+              </div>
+
+              <div class="form-group">
+                <input type="email" class="form-control" id="femail" placeholder="Email">
+              </div>
+
+              <div class="form-group">
+                <input type="text" class="form-control" id="fsubj" placeholder="Subject">
+              </div>
+
+            </div>
+            <div class="col-sm-6">
+              <div class="form-group">
+                <textarea class="form-control" rows="3" placeholder="Comment"></textarea>
+              </div>
+            </div>
+          </div>
+          <button type="button" class="btn btn-success pull-right text-uppercase comment-form-submit">Submit</button>
+        </form>
+
+      </div>
+    </div>
+  </section>
+
+  <?php
+  require_once('_footer.php');      

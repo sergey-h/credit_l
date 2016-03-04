da4ae29c068f6191c39261fc4d5f9811ed295212
sergey-h <ssd.hogan@gmail.com>
2016-03-04 20:29:47 +0300

    Added about & calc

---
+++ about.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>Welcome to Credit1Solutions.com</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap theme -->
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+  <link href="css/font-awesome.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+
+  <link href="scss/parts/fonts.css" rel="stylesheet">
+
+
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+
+  <link href="scss/parts/overall.css" rel="stylesheet">
+  <link href="scss/parts/header.css" rel="stylesheet">
+  <link href="scss/parts/footer.css" rel="stylesheet">
+
+  <link href="scss/pages/about.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script src="js/jquery.min.js"></script>
+</head>
+
+<body role="document">
+
+  <!-- Fixed navbar -->
+  <nav class="navbar">
+    <div class="brdrbtm">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-4 navbar-block">
+            <a class="navbar-brand" href="./"><img class="logo" src="images/logo.png" title="Credit1Solutions.com" alt="Credit1Solutions.com" /></a>
+          </div>
+          <div class="col-sm-8 navbar-block">
+            <div class="colnowsec flright">
+              <div class="callnow"><span class="telicon"></span> call now <span class="telno">1-877-782-7839</span></div>
+              <div class="wrap_getstart"><a href="#" class="getstarted">GET STARTED</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="flleft">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">SERVICES</a></li>
+                <li><a href="./">MILITARY DISCOUNTS</a></li>
+                <li><a href="./">CREDIT EDUCATION</a></li>
+              </ul>
+
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="flright search-block">
+            <div class="client-login">click here for the <span>Client Login</span></div>
+            <div class="search"><div id="input"><input type="search" name="search-terms" id="search-terms" placeholder="Enter search terms..."></div><span class="fa fa-search" id="label"></span></div>
+
+          </div>
+        </div>
+      </div><!-- end row-->
+
+    </div>
+  </nav>
+  <section class="banner banner-style1" style="background-image: url(images/banner2.jpg);">
+    <div class="container">
+      <div class="banner-content">
+        <h1>Why the credit 1 Solutions?</h1>
+        <p>Our client’s average  18.7 removals in 4 months! This is amongst the best credit repair results in the industry.....</p>
+        <button class="learnmore">Become A Client</button>
+      </div>
+    </div>
+  </section>
+  <section class="strapline">
+    <div class="container">
+      Credit Report Repair Questions? <span>Chat with a Certified FICO Expert Now: 1-877-782-7839</span>
+    </div>
+  </section>
+  <section class="about-page">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+
+          <ul class="list-group list-style1">
+            <li class="list-group-item"><a href="#">What We Do?</a></li>
+            <li class="list-group-item"><a href="#">What It Coast?</a></li>
+            <li class="list-group-item"><a href="#">Credit Cards</a></li>
+            <li class="list-group-item"><a href="#">Automobile Financing</a></li>
+            <li class="list-group-item"><a href="#">Home Mortgage</a></li>
+            <li class="list-group-item"><a href="#">Is Credit Repair Legal?</a></li>
+            <li class="list-group-item"><a href="#">Is Credit Repair for me?</a></li>
+            <li class="list-group-item"><a href="#">Credit Repair Act</a></li>
+            <li class="list-group-item"><a href="#">Contact Us</a></li>
+          </ul>  
+
+          <div class="about-testimonal">
+            <div id="carousel-about-testimonal" class="carousel slide">
+              <ol class="carousel-indicators">
+                <li data-target="#carousel-about-testimonal" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-about-testimonal" data-slide-to="1"></li>
+                <li data-target="#carousel-about-testimonal" data-slide-to="2"></li>
+                <li data-target="#carousel-about-testimonal" data-slide-to="3"></li>
+              </ol>   
+              <div class="carousel-inner" role="listbox"> 
+                <div class="item active">
+                  <div class="about-testimonal-body">
+                    <p>THANK YOU! Is there any way  you guys could mail me some of  your business cards because I  have 3 people that could use you  and I would like to</p>        
+                  </div>
+                  <div class="about-testimonal-author">- Nathan, Littleton, CO</div>
+                </div>
+                <div class="item">
+                  <div class="about-testimonal-body">
+                    <p>THANK YOU! Is there any way  you guys could mail me some of  your business cards because I  have 3 people that could use you  and I would like to</p>        
+                  </div>
+                  <div class="about-testimonal-author">- Nathan, Littleton, CO</div>
+                </div>     
+                <div class="item">
+                  <div class="about-testimonal-body">
+                    <p>THANK YOU! Is there any way  you guys could mail me some of  your business cards because I  have 3 people that could use you  and I would like to</p>        
+                  </div>
+                  <div class="about-testimonal-author">- Nathan, Littleton, CO</div>
+                </div> 
+                <div class="item">
+                  <div class="about-testimonal-body">
+                    <p>THANK YOU! Is there any way  you guys could mail me some of  your business cards because I  have 3 people that could use you  and I would like to</p>        
+                  </div>
+                  <div class="about-testimonal-author">- Nathan, Littleton, CO</div>
+                </div>                                            
+              </div>           
+            </div>
+          </div>
+
+        </div>
+        <div class="col-sm-8" >
+          <div class="about-content-item">
+            <h3 class="title-h1">What We Do</h3>  
+            <img src="images/tmp/about-img1.jpg" alt="" class="pull-right"> 
+            <p>Credit1Solutions.com is not a high pressure sales team that will make a lot of promises that can not be met. When you contact Credit1Solutions.com you will be speaking with a trained professional that will give you an honest assessment of your current credit situation. With your help we will identify what your goals are for your credit report and determine what we can do to put you in the best position possible</p>
+
+            <p> If you decide to move forward we will begin the process of challenging any inaccuracies on your current credit report. The credit bureaus have 30 days to investigate and respond to any challenges which typically results in deletions and improved scores. This process may take as little as 6 to 12 months; every case is unique.</p>  
+            <a href="#" class="read-more">READ MORE</a>       
+          </div>
+
+          <div class="about-content-item-x2 row">
+            <div class="about-content-item col-sm-6">
+              <h3 class="title-h1">What it Cost</h3>   
+              <div class="media">
+                <div class="media-left">
+                  <img class="media-object" src="images/tmp/about-img3.jpg"  alt="">
+                </div>
+                <div class="media-body">
+                  <p>As you consider working with Credit1Solutions.com you would do well to look at the price you are already paying for bad credit</p>
+                  <a href="#" class="read-more">READ MORE</a>  
+                </div>
+              </div>          
+
+            </div>
+            <div class="about-content-item col-sm-6">
+              <h3 class="title-h1">Credit Cards</h3> 
+              <div class="media">
+                <div class="media-left">
+                  <img class="media-object" src="images/tmp/about-img2.jpg"  alt="">
+                </div>
+                <div class="media-body">
+                  <p>Most if not all prime credit cards are entirely out of reach to consumers with bad credit.</p>
+                  <a href="#" class="read-more">READ MORE</a>  
+                </div>
+              </div>               
+            </div>
+          </div>
+
+          <div class="about-content-item">
+            <h3 class="title-h1">Automobile Financing</h3>  
+            <img src="images/tmp/about-img4.jpg" alt="" class="pull-right"> 
+            <p>If you are making payments on a car, you are probably paying between $5,000 and $9,000 more just for having bad credit. This added interest shows up every month in a higher payment. Take a look</p>
+
+            <a href="#" class="read-more">READ MORE</a>       
+          </div>
+
+          <div class="about-content-item">
+            <h3 class="title-h1">What We Do</h3>  
+            <img src="images/tmp/about-img5.jpg" alt="" class="pull-left"> 
+            <p>Bad credit in auto financing can really hurt, but it is nothing compared to the cost of bad credit when a home is involved. A typical home can cost between $50,000 and $130,000 more in interest if you are buying the home with bad credit. </p>
+
+            <a href="#" class="read-more">READ MORE</a>       
+          </div>    
+
+          <div class="about-content-item-x2 row">
+            <div class="about-content-item col-sm-6">
+              <h3 class="title-h1">Is Credit Repair Legal</h3>   
+              <div class="media">
+                <div class="media-left">
+                  <img class="media-object" src="images/tmp/about-img6.jpg"  alt="">
+                </div>
+                <div class="media-body">
+                  <p>Although the credit bureaus would like to have you think otherwise, there is absolutely nothing illegal about disputing questionable items on your credit report.</p>
+                  <a href="#" class="read-more">READ MORE</a>  
+                </div>
+              </div>          
+
+            </div>
+            <div class="about-content-item col-sm-6">
+              <h3 class="title-h1">Is Credit Repair For Me</h3> 
+              <div class="media">
+                <div class="media-left">
+                  <img class="media-object" src="images/tmp/about-img7.jpg"  alt="">
+                </div>
+                <div class="media-body">
+                  <p>Bankruptcies-Repossessions-Collections-Foreclosures-Charge Offs-Judgments, these are things on your credit report that can effect you.</p>
+                  <a href="#" class="read-more">READ MORE</a>  
+                </div>
+              </div>               
+            </div>
+          </div>  
+
+          <div class="about-content-item">
+            <h3 class="title-h1">Contact Us</h3>  
+            <p>The sooner the better is the truth with Credit Repair. Contact us by clicking here or Sign up online or call <span class="text-uppercase" style='color: #da6530'>Toll Free at 1-877-782-7839 or at (270) 982-4747.</span> </p>
+
+          </div>                                   
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <?php
+  require_once('_footer.php');     
---
+++ calculator.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>Welcome to Credit1Solutions.com</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap theme -->
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+  <link href="css/font-awesome.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+
+  <link href="scss/parts/fonts.css" rel="stylesheet">
+
+
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+
+  <link href="scss/parts/overall.css" rel="stylesheet">
+  <link href="scss/parts/header.css" rel="stylesheet">
+  <link href="scss/parts/footer.css" rel="stylesheet">
+
+  <link href="scss/pages/faq.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script src="js/jquery.min.js"></script>
+</head>
+
+<body role="document" class='page-blog'>
+
+  <!-- Fixed navbar -->
+  <nav class="navbar">
+    <div class="brdrbtm">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-4 navbar-block">
+            <a class="navbar-brand" href="./"><img class="logo" src="images/logo.png" title="Credit1Solutions.com" alt="Credit1Solutions.com" /></a>
+          </div>
+          <div class="col-sm-8 navbar-block">
+            <div class="colnowsec flright">
+              <div class="callnow"><span class="telicon"></span> call now <span class="telno">1-877-782-7839</span></div>
+              <div class="wrap_getstart"><a href="#" class="getstarted">GET STARTED</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="flleft">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">SERVICES</a></li>
+                <li><a href="./">MILITARY DISCOUNTS</a></li>
+                <li><a href="./">CREDIT EDUCATION</a></li>
+              </ul>
+
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="flright search-block">
+            <div class="client-login">click here for the <span>Client Login</span></div>
+            <div class="search"><div id="input"><input type="search" name="search-terms" id="search-terms" placeholder="Enter search terms..."></div><span class="fa fa-search" id="label"></span></div>
+
+          </div>
+        </div>
+      </div><!-- end row-->
+
+    </div>
+  </nav>
+
+  <section class="breadcrumbs"> 
+    <div class="container">
+      <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Calculator</li>
+      </ol>
+    </div>
+  </section>
+
+  <section class="page-heading">
+    <div class="container">
+      <h1 class="title-h1">Calculators</h1>
+      <div class="media media-style3">
+        <div class="media-left hidden-xs">
+          <a href="#">
+            <img class="media-object" src="images/header-calc.png" alt="">
+          </a>
+        </div>
+        <div class="media-body">
+          <h2>Please feel free to use these self help calculators</h2>
+          <p>These are a good tools to help you Manage Your Assets, Savings and Retirement, Major Purchases, and Credit Card Payoff.</p>        
+        </div>
+      </div>      
+    </div>
+  </section>
+
+  <section class="strapline">
+    <div class="container">
+      Credit Report Repair Questions? <span>Chat with a Certified FICO Expert Now: 1-877-782-7839</span>
+    </div>
+  </section>
+
+
+  <section class="faq-body">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-5">
+          <ul class="list-group list-heading-context">
+            <li class="list-group-item item-header"><span>Calculators</span></li>
+            <li class="list-group-item item-active">
+              <a href="" class="active">Manage Your Assets</a>
+              <div class="faq-item visible-xs-block">
+                <img class="faq-featured-image" src="images/calc-img.png" alt="">
+                <div class="faq-content">
+                  <p>Home Budget - Wondering where your money goes? Analyze your budget and find out.</p>
+
+                  <p>Net Worth - This calculator helps you determine your net worth.
+                    Should You Refinance your Mortgage? Use this calculator to see whether or not you could save money by refinancing your mortgage.</p>            
+                  </div>
+              </div>
+            </li>
+            <li class="list-group-item"><a href="">Savings and Retirement</a></li>
+            <li class="list-group-item"><a href="">Major Purchases</a></li>
+            <li class="list-group-item"><a href="">Credit Cards</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-7 hidden-xs">
+          <div class="faq-item" id="faq-placeholder">
+            <h1 class="title-h1 faq-title">Manage Your Assets</h1>
+            <img class="faq-featured-image" src="images/calc-img.png" alt="">
+            <div class="faq-content">
+
+              <p>Home Budget - Wondering where your money goes? Analyze your budget and find out.</p>
+
+              <p>Net Worth - This calculator helps you determine your net worth.
+                Should You Refinance your Mortgage? Use this calculator to see whether or not you could save money by refinancing your mortgage.</p>
+
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php
+  require_once('_footer.php');      

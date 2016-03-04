7f07e1058fb6a41e030dcd89b2da5bbd5ab848a3
sergey-h <ssd.hogan@gmail.com>
2016-03-04 16:48:27 +0300

    Some pages added

---
+++ business.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>Welcome to Credit1Solutions.com</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap theme -->
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+  <link href="css/font-awesome.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+
+  <link href="scss/parts/fonts.css" rel="stylesheet">
+
+
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+
+  <link href="scss/parts/overall.css" rel="stylesheet">
+  <link href="scss/parts/header.css" rel="stylesheet">
+  <link href="scss/parts/footer.css" rel="stylesheet">
+
+  <link href="scss/pages/common-q.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script src="js/jquery.min.js"></script>
+</head>
+
+<body role="document" class='page-blog'>
+
+  <!-- Fixed navbar -->
+  <nav class="navbar">
+    <div class="brdrbtm">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-4 navbar-block">
+            <a class="navbar-brand" href="./"><img class="logo" src="images/logo.png" title="Credit1Solutions.com" alt="Credit1Solutions.com" /></a>
+          </div>
+          <div class="col-sm-8 navbar-block">
+            <div class="colnowsec flright">
+              <div class="callnow"><span class="telicon"></span> call now <span class="telno">1-877-782-7839</span></div>
+              <div class="wrap_getstart"><a href="#" class="getstarted">GET STARTED</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="flleft">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">SERVICES</a></li>
+                <li><a href="./">MILITARY DISCOUNTS</a></li>
+                <li><a href="./">CREDIT EDUCATION</a></li>
+              </ul>
+
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="flright search-block">
+            <div class="client-login">click here for the <span>Client Login</span></div>
+            <div class="search"><div id="input"><input type="search" name="search-terms" id="search-terms" placeholder="Enter search terms..."></div><span class="fa fa-search" id="label"></span></div>
+
+          </div>
+        </div>
+      </div><!-- end row-->
+
+    </div>
+  </nav>
+
+  <section class="breadcrumbs"> 
+    <div class="container">
+      <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Common Questions</li>
+      </ol>
+    </div>
+  </section>
+
+  <nav class="navbar-second">
+    <div class="container">
+      <ul class="nav nav-pills">
+        <li role="presentation"><a href="#">Contact Form</a></li>
+        <li role="presentation" class="active" ><a href="#">Business Listing</a></li>
+        <li role="presentation" ><a href="#">Get Directions</a></li>
+      </ul>
+    </div>
+
+  </nav>
+
+  <section class="page-heading">
+    <div class="container">
+      <div class="media-style2">
+        <h1>Who we are</h1>
+        <p>Credit1solutions.com insn't just our name; its a title we have earned. We are credit experts to guide and assist you.</p>        
+      </div>      
+    </div>
+  </section>
+
+  <section class="strapline">
+    <div class="container">
+      Credit Report Repair Questions? <span>Chat with a Certified FICO Expert Now: 1-877-782-7839</span>
+    </div>
+  </section>
+
+
+  <section class="common-questions">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm-6 cq-info">
+          <div class="row">
+            <div class="col-xs-12 col-sm-offset-1">        
+              <h3>Information</h3>
+              <div class="address">
+                <span class="fa fa-phone"></span> Phone: (877) 782-7839 <br>
+                <span class="fa fa-street-view"></span> 5284 N Dixie Hwy Elizabethtown, KY 42701            
+              </div>
+            </div>          
+          </div>          
+        </div>
+        <div class="col-sm-6 cq-bhour">                   
+          <div class="row">
+            <div class="col-xs-12 col-sm-offset-1">
+              <h3>Business Hours</h3>               
+              <table class="table">
+                <tr>
+                  <td>Monday</td>
+                  <td>9:00am - 6:00pm</td>
+                </tr>
+                <tr>
+                  <td>Tuesday</td>
+                  <td>9:00am - 6:00pm</td>
+                </tr>
+                <tr>
+                  <td>Wednesday</td>
+                  <td>9:00am - 6:00pm</td>
+                </tr>
+                <tr>
+                  <td>Thursday</td>
+                  <td>9:00am - 6:00pm</td>
+                </tr>
+                <tr>
+                  <td>Friday</td>
+                  <td>9:00am - 6:00pm</td>
+                </tr>
+                <tr>
+                  <td>Saturday</td>
+                  <td>Closed</td>
+                </tr>
+                <tr>
+                  <td>Sunday</td>
+                  <td>Closed</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row text-center">
+        <button class="orangebtn orangebtn-big text-uppercase">credit1solutions.com</button>
+      </div>
+    </div>
+  </section>
+
+  <?php
+  require_once('_footer.php');      
---
+++ directions.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>Welcome to Credit1Solutions.com</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap theme -->
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+  <link href="css/font-awesome.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+
+  <link href="scss/parts/fonts.css" rel="stylesheet">
+
+
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+
+  <link href="scss/parts/overall.css" rel="stylesheet">
+  <link href="scss/parts/header.css" rel="stylesheet">
+  <link href="scss/parts/footer.css" rel="stylesheet">
+
+  <link href="scss/pages/common-q.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script src="js/jquery.min.js"></script>
+</head>
+
+<body role="document" class='page-blog'>
+
+  <!-- Fixed navbar -->
+  <nav class="navbar">
+    <div class="brdrbtm">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-sm-4 navbar-block">
+            <a class="navbar-brand" href="./"><img class="logo" src="images/logo.png" title="Credit1Solutions.com" alt="Credit1Solutions.com" /></a>
+          </div>
+          <div class="col-sm-8 navbar-block">
+            <div class="colnowsec flright">
+              <div class="callnow"><span class="telicon"></span> call now <span class="telno">1-877-782-7839</span></div>
+              <div class="wrap_getstart"><a href="#" class="getstarted">GET STARTED</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="flleft">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">SERVICES</a></li>
+                <li><a href="./">MILITARY DISCOUNTS</a></li>
+                <li><a href="./">CREDIT EDUCATION</a></li>
+              </ul>
+
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="flright search-block">
+            <div class="client-login">click here for the <span>Client Login</span></div>
+            <div class="search"><div id="input"><input type="search" name="search-terms" id="search-terms" placeholder="Enter search terms..."></div><span class="fa fa-search" id="label"></span></div>
+
+          </div>
+        </div>
+      </div><!-- end row-->
+
+    </div>
+  </nav>
+
+  <section class="breadcrumbs"> 
+    <div class="container">
+      <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Common Questions</li>
+      </ol>
+    </div>
+  </section>
+
+  <nav class="navbar-second">
+    <div class="container">
+      <ul class="nav nav-pills">
+        <li role="presentation" ><a href="#">Contact Form</a></li>
+        <li role="presentation" ><a href="#">Business Listing</a></li>
+        <li role="presentation" class="active" ><a href="#">Get Directions</a></li>
+      </ul>
+    </div>
+
+  </nav>
+
+  <section class="page-heading">
+    <div class="container">
+      <div class="media-style2">
+        <h1>Credit1solutions.com</h1>
+        <p>Let's Get Started, We'll respond quickly to your request.</p>        
+      </div>      
+    </div>
+  </section>
+
+  <section class="strapline">
+    <div class="container">
+      Credit Report Repair Questions? <span>Chat with a Certified FICO Expert Now: 1-877-782-7839</span>
+    </div>
+  </section>
+
+
+  <section class="common-questions">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="get_directions">
+            <div class="row">
+              <div class="col-sm-3 text-right">Get Directions</div>
+              <div class="col-sm-9">                
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search for...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">Go</button>
+                  </span>
+                </div>                
+              </div>
+            </div>        
+          </div>
+            <div class="row get_directions_map embed-responsive embed-responsive-16by9">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12616.761869277927!2d-85.90537217730449!3d37.762131932774345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8868e7b77c720ae1%3A0x4c343f473a8114a4!2zNTI4NCBOIERpeGllIEh3eSwgRWxpemFiZXRodG93biwgS1kgNDI3MDEsINCh0KjQkA!5e0!3m2!1sru!2sby!4v1457096829399" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>            
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <?php
+  require_once('_footer.php');      
